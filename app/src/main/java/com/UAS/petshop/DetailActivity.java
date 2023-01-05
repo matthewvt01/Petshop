a47f9df4f6439c07bb8b9fbf654045e5bac34ef9
matthewvt01 <matthewvincentius01@gmail.com>
2023-01-05 11:22:57 +0700

    update 2

--- app/src/main/java/com/UAS/petshop/DetailActivity.java
+++ app/src/main/java/com/UAS/petshop/DetailActivity.java
@@ -3,7 +3,10 @@ package com.UAS.petshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,6 +14,7 @@ import com.bumptech.glide.Glide;
 
 public class DetailActivity extends AppCompatActivity {
     private ImageView ivFoto;
+    private Button btnBukaLokasi;
     private TextView tvNama, tvTentang, tvAlamat, tvKoordinat;
     private String yNama, yTentang, yFoto, yAlamat, yKoordinat;
 
@@ -19,11 +23,14 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+
+
         ivFoto = findViewById(R.id.iv_foto);
         tvNama = findViewById(R.id.tv_nama);
         tvTentang = findViewById(R.id.tv_tentang);
         tvAlamat = findViewById(R.id.tv_alamat);
         tvKoordinat = findViewById(R.id.tv_koordinat);
+        btnBukaLokasi = findViewById(R.id.btn_buka_lokasi);
 
         Intent terima = getIntent();
         yNama = terima.getStringExtra("xNama");
@@ -37,5 +44,19 @@ public class DetailActivity extends AppCompatActivity {
         tvAlamat.setText(yAlamat);
         tvKoordinat.setText(yKoordinat);
         Glide.with(DetailActivity.this).load(yFoto).into(ivFoto);
+
+        btnBukaLokasi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uribukalokasi = Uri.parse("geo:0,0?q=" + yKoordinat);
+
+                Intent bukalokasi = new Intent(Intent.ACTION_VIEW, uribukalokasi);
+                startActivity(bukalokasi);
+            }
+        });
+
+
+
     }
+
 }
